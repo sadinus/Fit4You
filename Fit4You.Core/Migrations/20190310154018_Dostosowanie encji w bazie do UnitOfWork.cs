5e1dcd0c49a8c1a49e68df277d99b63bd364101a
Pindelski, Paweł <p_pindelski@o2.pl>
2019-03-10 16:41:03 +0100

    Migracja: Zmiana pól nazw pól Id w encjach

---
+++ Fit4You.Core/Migrations/20190310154018_Dostosowanie encji w bazie do UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fit4You.Core.Migrations
+{
+    public partial class DostosowanieencjiwbaziedoUnitOfWork : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserDataID",
+                table: "UserData",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "UserID",
+                table: "User",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "UserData",
+                newName: "UserDataID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "User",
+                newName: "UserID");
+        }
+    }
+}
