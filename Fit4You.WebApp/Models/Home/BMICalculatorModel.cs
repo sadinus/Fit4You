5834abef84afb0da4c2bcbd1cfaf33acf88e4fe0
Paweł Pindelski <snakepl_1993@o2.pl>
2019-03-18 21:20:27 +0100

    Dodanie inputów dla kalkulatorów

---
+++ Fit4You.WebApp/Models/Home/BMICalculatorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fit4You.WebApp.Models.Home
+{
+    public class BMICalculatorModel
+    {
+        public bool IsMan { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+    }
+}
