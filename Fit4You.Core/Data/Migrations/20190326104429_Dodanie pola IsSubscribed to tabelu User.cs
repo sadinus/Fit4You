c89def6b9a50138ad4476e1b12c786e46d437d5e
Pindelski, Paweł <p_pindelski@o2.pl>
2019-03-26 17:20:27 +0100

    Dodanie bublioteki boobstrap4-toggle + migracja

---
+++ Fit4You.Core/Data/Migrations/20190326104429_Dodanie pola IsSubscribed to tabelu User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fit4You.Core.Migrations
+{
+    public partial class DodaniepolaIsSubscribedtotabeluUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubscribed",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubscribed",
+                table: "User");
+        }
+    }
+}
--- Fit4You.WebApp/Controllers/AccountController.cs
+++ Fit4You.WebApp/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Fit4You.Core.Data;
+using Fit4You.Core.Data.Repositories;
 using Fit4You.Core.Domain;
 using Fit4You.Core.Services;
 using Fit4You.WebApp.Models;
@@ -15,12 +16,14 @@ namespace Fit4You.WebApp.Controllers
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
         private readonly ICalculatorService calculatorService;
+        private readonly User currentUser;
 
         public AccountController(IUnitOfWork unitOfWork, IMapper mapper, ICalculatorService calculatorService)
         {
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
             this.calculatorService = calculatorService;
+            currentUser = unitOfWork.UserRepository.GetById(Int32.Parse(User.FindFirst("id").Value));
         }
 
         public IActionResult Index()
@@ -30,8 +33,7 @@ namespace Fit4You.WebApp.Controllers
 
         public IActionResult UserInformation()
         {
-            var userId = Int32.Parse(User.FindFirst("id").Value);
-            var entity = unitOfWork.UserDataRepository.GetByUserId(userId);
+            var entity = unitOfWork.UserDataRepository.GetByUserId(currentUser.Id);
             UserInformationDisplayModel model;
             if (entity != null)
             {
@@ -44,14 +46,13 @@ namespace Fit4You.WebApp.Controllers
                 return View(model);
             }
 
-            model = new UserInformationDisplayModel("Unknown");
+            model = new UserInformationDisplayModel("Unknown", currentUser);
             return View(model);
         }
 
         public IActionResult UserInformationEdit()
         {
-            var userId = Int32.Parse(User.FindFirst("id").Value);
-            var entity = unitOfWork.UserDataRepository.GetByUserId(userId);
+            var entity = unitOfWork.UserDataRepository.GetByUserId(currentUser.Id);
 
             if (entity != null)
             {
@@ -67,19 +68,18 @@ namespace Fit4You.WebApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                var userId = Int32.Parse(User.FindFirst("id").Value);
-                var entity = unitOfWork.UserDataRepository.GetByUserId(userId);
+                var entity = unitOfWork.UserDataRepository.GetByUserId(currentUser.Id);
 
                 if (entity != null)
                 {
                     mapper.Map<UserInformationModel, UserData>(model, entity);
                     unitOfWork.UserDataRepository.Update(entity);
-
                 }
                 else
                 {
+                    entity = new UserData();
                     mapper.Map<UserInformationModel, UserData>(model, entity);
-                    entity.UserID = userId;
+                    entity.UserID = currentUser.Id;
                     unitOfWork.UserDataRepository.Add(entity);
                 }
 
