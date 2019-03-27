1cdbe875ca8b013aa7817ba634a4a9930398a5a6
Pindelski, Pawel <snakepl_1993@o2.pl>
2019-03-27 11:21:20 +0100

    Zainstalowanie biblioteki Quartz.net + utworzenie JobScheduler'a

---
+++ Fit4You.Core/Services/IMailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fit4You.Core.Services
+{
+    public interface IMailService
+    {
+        void SendNewsletterToSubscribedUsers();
+    }
+}
