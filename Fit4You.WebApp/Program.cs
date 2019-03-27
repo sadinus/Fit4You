d12e4b82ca03337c36d6784cdeb702c1e94a1b82
Pindelski, Pawel <snakepl_1993@o2.pl>
2019-03-27 15:06:15 +0100

    Przeniesienie MailHostedServices do folderu z serwisami + usunięcie zbędnych usingów

--- Fit4You.WebApp/Program.cs
+++ Fit4You.WebApp/Program.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Fit4You.Core.Utilities;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Fit4You.WebApp
 {
