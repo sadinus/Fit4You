fe4299ecd40866d8331fc147de11af9d911f33c0
Paweł Pindelski <snakepl_1993@o2.pl>
2019-03-28 21:43:01 +0100

    Ustawienie UnitOfWork i Fit4YouContext jako Scoped

---
+++ Fit4You.Core/Utilities/ScopedMailService.cs
@@ -0,0 +1,22 @@
+﻿using Fit4You.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fit4You.Core.Utilities
+{
+    public class ScopedMailService : IScopedMailService
+    {
+        private readonly IMailService mailService;
+
+        public ScopedMailService(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+
+        public void DoWork()
+        {
+            mailService.SendNewsletterToSubscribedUsers();
+        }
+    }
+}
